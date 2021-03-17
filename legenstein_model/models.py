24d955d88d7f39be682dc48fa73c9320ecc5abd1
t-cormier <cormier.titouan@gmail.com>
2021-03-17 19:16:24 +0100

    cn search issue

--- legenstein_model/models.py
+++ legenstein_model/models.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 def pseudo_derivative(v_scaled, dampening_factor): # checked
     return dampening_factor * tf.maximum(1 - tf.abs(v_scaled), 0)
 
-
+# TODO: stochastic spiking
 @tf.custom_gradient
 def spike_function(v_scaled, dampening_factor): # checked
     z_ = tf.greater(v_scaled, 0.)
@@ -93,17 +93,18 @@ class CellConstraint(keras.constraints.Constraint):
         return w
 
 
+
 class LIFCell(layers.Layer):
     """RSNN model for the Experiemnt (LIF)"""
 
-    def __init__(self, units, tau=20., tau_readout=50., thr=1., dt=1, n_refractory=5, dampening_factor=.3):
+    def __init__(self, units, tau=20., thr=1., dt=1, n_refractory=5, wmax=1., dampening_factor=.3):
         super().__init__()
         self.units = units
 
         self._dt = float(dt)
         self._decay = tf.exp(-dt / tau)
-        self._readout_decay = tf.exp(-dt / tau_readout)
         self._n_refractory = n_refractory
+        self._wmax = wmax
 
 
         self.threshold = thr
@@ -179,7 +180,7 @@ def compute_avg_activity(model, z):
     av = tf.reduce_mean(z, axis=(0, 1))
     return av # shape=(1,100)
 
-
+# TODO: PAPER etrace
 def compute_etrace(model, v, z):
 
     v_scaled = tf.identity((v - model.cell.threshold) / model.cell.threshold, name='v_scaled')
@@ -197,13 +198,59 @@ def compute_etrace(model, v, z):
     # Warning : Only LTP implemented here
     return eligibility_traces_convolved_w_rec #shape=(None, 1000, 100, 100)
 
+def leg_etrace(model, z):
+    wmax = model.cell._wmax
+    shp = z.get_shape()
+    batch_size = shp[1]
+
+    # useful functions
+    def w_plus(delta_t, A_p=0.01*wmax, tau_p=30):
+        return A_p * tf.exp(-delta_t/tau_p)
+
+    def w_minus(delta_t, A_m=0.01*wmax*1.05, tau_m=30):
+        return A_m * tf.exp(delta_t/tau_m)
+
+    def f_c(time, tau_e=400):
+        return time/tau_e * tf.exp(time/tau_e)
+
+    def vector_shape(vector):
+        return tf.reshape(vector, [1, int(vector.shape[1]), 1])
+
+    def kernel_shape(kernel):
+        return tf.reshape(kernel, [int(kernel.shape[0]), 1, 1])
+
+    # lets compute the LTP and LTD vectors
+    W_p = tf.stack([w_plus(r) for r in range(batch_size)]) # (700,)
+    W_m = tf.stack([w_minus(r) for r in range(batch_size)]) # (700,)
+
+    # let's compute the STDP weight change to be stored in the etrace
+    w_p = tf.stack( [ tf.nn.conv1d(vector_shape(z_j), kernel_shape(W_p), 1, padding='SAME') for z_j in tf.unstack(z, axis=2)  ] , axis=2) # (1, 700, 100, 1)
+    w_m = tf.stack( [ tf.nn.conv1d(vector_shape(z_j), kernel_shape(W_m), 1, padding='SAME') for z_j in tf.unstack(z, axis=2)  ] , axis=2) # (1, 700, 100, 1)
+
+    iter_p = zip(  tf.unstack(w_p, axis=1), tf.unstack(z, axis=1) )
+    iter_m = zip(  tf.unstack(w_m, axis=1), tf.unstack(z, axis=1) )
+
+    w_p = tf.stack([  tf.matmul(w_p_t, z_t[None,:,:]) for w_p_t, z_t in iter_p  ], axis=1 ) # (1, 700, 100, 100)
+    w_m = tf.stack([  tf.matmul(w_m_t, z_t[None,:,:]) for w_m_t, z_t in iter_m  ], axis=1 ) # (1, 700, 100, 100)
+
+    w = w_p + tf.transpose(w_m , perm = [0, 1, 3, 2])
+
+    # let's store the STDP changes in the e_trace
+    fc = tf.stack( [ f_c(s) for s in range(batch_size) ] ) # (700,)
+    fc_k = kernel_shape(fc)
+
+    etrace = tf.stack([
+        tf.stack([
+            tf.nn.conv1d(vector_shape(elem_ji), fc_k, 1, padding='SAME')  for elem_ji in tf.unstack(elem_j, axis=2)
+        ], axis=2)  for  elem_j in tf.unstack(w, axis=2)
+    ], axis=2) # (1, 700, 100, 100, 1)
+
+    etrace = tf.reshape(etrace, etrace.shape[:-1]) # (1, 700, 100, 100)
+
+    return etrace
 
 
-
-
-
-
-def compute_dopamine(idx_cn, z):
+def leg_dopamine(idx_cn, z):
     # switch to time major
     shp = z.get_shape()
     seq_len = shp[1]
@@ -225,46 +272,51 @@ def compute_dopamine(idx_cn, z):
     r_k = tf.reshape(r_k, [int(r_k.shape[0]), 1, 1], name='reward_kernel')
 
     dopa = tf.nn.conv1d(z_cn, r_k, stride=1, padding='SAME')
+    dopa = tf.reshape(dopa, dopa.shape[:-1])
 
-    return dopa #shape=(1,1000)
+    return dopa #shape=(1,700)
 
 
-def reg_loss(z, cn_idx, target_rate):
-    av = tf.reduce_mean(z, axis=(0, 1))
-    average_firing_rate_error = target_rate - av[cn_idx]
-    regularization_loss = tf.maximum(average_firing_rate_error, 0)
-    return regularization_loss
+def leg_gradients(dopamine, etrace):
+    grads =  tf.stack([
+        tf.stack([
+            dopamine * etrace_ji for etrace_ji in tf.unstack(etrace_i, axis=2)
+        ], axis=2) for etrace_i in tf.unstack(etrace, axis=2)
+    ], axis=2) # (1, 700, 100, 100)
+    dw_rec_dt = tf.reduce_sum(grads, axis=[0,1]) # (1,100,100)
+    return [-dw_rec_dt]
 
 
 ############### metrics ###################
 class Activity_metric(tf.keras.metrics.Metric):
     """ Activity metrics for the conditioned neuron and the whole network """
-    def __init__(self, cn_idx=None, name="activity_metric", **kwargs):
+    def __init__(self, cn_bool=False, name="activity_metric", **kwargs):
         super(Activity_metric, self).__init__(name=name, **kwargs)
-        self.cn_idx = cn_idx
+        self.cn_bool = cn_bool
         self.avg_act = None
         self.avg_act_cn = None
 
 
-    def update_state(self, y_true, z_pred, sample_weight=None):
+    def update_state(self, cn_idx, z_pred, sample_weight=None):
         avg_activity = tf.reduce_mean(z_pred, axis=(0, 1))
-        if self.cn_idx == None :
-            self.avg_act = tf.reduce_mean(avg_activity)
+        if self.cn_bool :
+            self.avg_act_cn = avg_activity[cn_idx]
+
         else :
-            self.avg_act_cn = avg_activity[self.cn_idx]
+            self.avg_act = tf.reduce_mean(avg_activity)
             #assert self.avg_act_cn > 0, "Conditioned neuron has no spontaneous activity"
 
     def result(self):
-        if self.cn_idx == None :
-            return self.avg_act
-        else :
+        if self.cn_bool :
             return self.avg_act_cn
-
-    def reset_states(self):
-        if self.cn_idx == None :
-            self.avg_act = 0.
         else :
-            self.avg_act_cn = 0.
+            return self.avg_act
+
+    # def reset_states(self):
+    #     if self.cn_bool:
+    #         self.avg_act_cn = 0.
+    #     else :
+    #         self.avg_act = 0.
 
 
 
@@ -286,6 +338,8 @@ class Exp_model(keras.Model):
         self.init_volt = tf.Variable(self.cell.zero_state()[0], trainable=False, name='init_voltage')
         self.init_refrac = tf.Variable(self.cell.zero_state()[1], trainable=False, name='init_refractory')
         self.init_spike = tf.Variable(self.cell.zero_state()[2], trainable=False, name='init_spike')
+        self.cn = tf.Variable(101, trainable=False, dtype=tf.int32)
+        self.cn_search = True
 
 
     def call(self, inputs):
@@ -303,45 +357,49 @@ class Exp_model(keras.Model):
 class Leg_fit(keras.Model):
     """Custom model.fit for (Legenstein and al., 2008) learning rule"""
 
-    def __init__(self, model, cn_idx, target_rate=0.0001):
+    def __init__(self, model):
         super(Leg_fit, self).__init__()
         self.model = model
-        assert cn_idx in np.arange(self.model.cell.units)
-        self.cn = cn_idx
-        self.target_rate = target_rate
-        self.stop_training = False
 
 
     def train_step(self, data):
         x, y = data
-        with tf.GradientTape() as tape :
-            v, z = self.model(x)
-            regularization_loss_cn = reg_loss(z, self.cn, target_rate=self.target_rate)
-
-        #self.metrics.reset_states()
-        self.compiled_metrics.update_state(y, z)
-
-        # compute the gradients ( grad = - delta w_ji = - d(t) * e_ji )
+        v, z = self.model(x) # z : (1, 700, 100)
         vars = self.model.trainable_variables
 
-        dopa = compute_dopamine(self.cn, z)
+        if self.model.cn_search :
+            avg_act = tf.reduce_mean(z, axis=(0,1)) # (100)
+            cond = tf.logical_and(avg_act > 0.008, avg_act < 0.013) # (1,100)
+            assert_cn = tf.debugging.assert_none_equal(tf.reduce_sum(tf.cast(cond, tf.int32)), 0, message='No neuron suitable, reroll')
+            eligible = tf.cast(tf.where(cond), tf.int32)[0]
 
-        etrace = compute_etrace(self.model, v, z)
-        leg_grads = tf.reduce_sum(dopa[:, :, None, None] * etrace, axis=(0, 1), name='leg_grads')
-        reg_grads = tape.gradient(regularization_loss_cn, vars)
-        grads = reg_grads # + leg_grads
+            self.model.cn.assign(eligible[0])
+            self.model.cn_search = False
+            tf.debugging.assert_none_equal(self.model.cn.value(), 101, message='error 1 : cn idx not assigned')
+            self.compiled_metrics.update_state(self.model.cn.value(), z)
 
-        # show the gradients as Metrics
-        metric_leg_grads = tf.reduce_mean(leg_grads)
-        metric_reg_grads = tf.reduce_mean(tf.math.abs(reg_grads))
+            fake_grads = [tf.zeros(shape=(self.model.cell.units, self.model.cell.units))]
+            self.optimizer.apply_gradients(zip(fake_grads, vars))
 
-        # Apply the gradients
-        self.optimizer.apply_gradients(zip(grads, vars))
+            return {'CN average activity first batch' : self.metrics[0].result(),
+                    'Chosen one' : eligible[0]}
 
-        return {'CN average activity' : self.metrics[0].result(),
-                'CN regularization loss' : regularization_loss_cn,
-                'average network activity' : self.metrics[1].result()}
-                # 'Leg grads' : metric_leg_grads,
-                # 'Reg grads' : metric_reg_grads}
+        else :
+            #self.metrics.reset_states()
+            tf.debugging.assert_none_equal(self.model.cn.value(), 101, message='error 2 : cn idx not assigned')
+            self.compiled_metrics.update_state(self.model.cn.value(), z)
+            # compute the gradients ( grad = - delta w_ji = - d(t) * e_ji )
+            print('\rGraph computing .     ', end='')
+            dopa = leg_dopamine(self.model.cn.value(), z)
+            print('\rGraph computing . .   ', end='')
+            etrace = leg_etrace(self.model, z)
+            print('\rGraph computing . . . ', end='')
+            grads = leg_gradients(dopa, etrace)
+            print('\rGraph computed        ')
+            # show the gradients as Metrics
+            metric_leg_grads = tf.reduce_mean(grads)
+            # Apply the gradients
+            self.optimizer.apply_gradients(zip(grads, vars))
 
-        #return {m.name: m.result() for m in self.metrics}
+            return {'CN average activity' : self.metrics[0].result(),
+                    'average network activity' : self.metrics[1].result()}
